e8b54e664e2d8b9063fe0df5733360e7592a0b4d
Christian <christian@nyu.edu>
2019-09-18 12:02:27 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Texas!</h1>
+      <img className="App-logo" width="500" alt="texas on the map of the united states" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Texas_in_United_States.svg/1200px-Texas_in_United_States.svg.png" />
+      <img className="blue-border" width="500" alt="a cow with a bell on sitting in a field" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/CH_cow_2_cropped.jpg/1280px-CH_cow_2_cropped.jpg"/>
     </div>
   );
 }
